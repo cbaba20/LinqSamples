829ce72baf215358b7ed4c5ca238930cb903030d
Chandan Kumar Baba <moodler18@gmail.com>
2017-11-06 18:58:07 +0530

    Added files made during learning of Linq

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSamples
+{
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var movies = new List<Movies>
+            {
+                new Movies {Title= "The Dark Knight Rises", Rating= 8.9f, Year=2007 },
+                new Movies {Title="The Immortals", Rating=7.6f, Year=1942 },
+                new Movies {Title="Schindlers List",Rating=8.9f, Year=1993 },
+                new Movies {Title="The Pianist",Rating=8.5f, Year=2002 }
+            };
+
+
+
+            //using extension method
+
+            var query = movies.Filter(m => m.Year > 2000);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
